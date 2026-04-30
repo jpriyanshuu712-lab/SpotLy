cb0e7eae8592e6604b34332a08ca41b8ef1ad5b0
jpriyanshuu712-lab <jpriyanshuu712@gmail.com>
2026-04-30 15:43:28 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,21 @@
+import pandas as pd
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+data = pd.read_csv("list 1.csv")
+
+@app.route("/")
+def home():
+    return "Spotly is running!"
+
+@app.route("/recommend", methods=["GET"])
+def recommend():
+    cuisine = request.args.get("cuisine")
+
+    results = data[data["Cuisine"].str.contains(cuisine, case=False, na=False)]
+
+    return jsonify(results.head(5).to_dict(orient="records"))
+
+if __name__ == "__main__":
+    app.run(debug=True)
