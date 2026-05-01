47cbc99ad873ae65d7fcf6047f47c7f12ce84776
jpriyanshuu712-lab <jpriyanshuu712@gmail.com>
2026-05-01 17:22:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,13 @@
+from flask import Flask, render_template, request, jsonify
+import pandas as pd
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
 @app.route("/recommend")
 def recommend():
     query = request.args.get("cuisine")
@@ -25,3 +35,7 @@ def recommend():
         })
 
     return jsonify(results)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
