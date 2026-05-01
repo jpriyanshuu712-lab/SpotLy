809c2625d016f913559d6776182d77fcb3613f7a
jpriyanshuu712-lab <jpriyanshuu712@gmail.com>
2026-05-01 17:16:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,39 +1,27 @@
-from flask import Flask, render_template, request, jsonify
-import pandas as pd
-
-app = Flask(__name__)
-
-# Load restaurant data
-df = pd.read_csv("restaurants.csv")
-
-@app.route("/")
-def home():
-    return render_template("index.html")
-
 @app.route("/recommend")
 def recommend():
+    query = request.args.get("cuisine")
 
-    query = request.args.get("cuisine").lower()
+    if not query:
+        return jsonify([])
+
+    df = pd.read_csv("restaurants.csv")
 
     filtered = df[
-    df["cuisine"].str.contains(query, case=False) |
-    df["location"].str.contains(query, case=False)
-]
+        df["Cuisine Type"].str.contains(query, case=False, na=False) |
+        df["Area / Location"].str.contains(query, case=False, na=False)
+    ]
 
     results = []
 
-    for _, row in filtered.head(10).iterrows():
-       results.append({
-    "name": row["Restaurant Name"],
-    "cuisine": row["Cuisine Type"],
-    "location": row["Area / Location"],
-    "rating": row["Rating"],
-    "price": row["Price Range (for 2)"],
-    "tags": row["Tags"]
-})
+    for _, row in filtered.iterrows():
+        results.append({
+            "name": row["Restaurant Name"],
+            "cuisine": row["Cuisine Type"],
+            "location": row["Area / Location"],
+            "rating": row["Rating"],
+            "price": row["Price Range (for 2)"],
+            "tags": row["Tags"]
+        })
 
     return jsonify(results)
-
-
-if __name__ == "__main__":
-    app.run(debug=True)
