82fb6596f4dc6d9281d255aff74a7b24505a7da6
jpriyanshuu712-lab <jpriyanshuu712@gmail.com>
2026-07-15 10:13:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-
 from flask import Flask, render_template, request, jsonify
+from flask_cors import CORS
 import pandas as pd
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/")
 def home():
@@ -50,3 +51,4 @@ def recommend():
 
 if __name__ == "__main__":
     app.run(debug=True)
+
